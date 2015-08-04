e2925182a6067ba98adef5a6cc8bb0dc0f09942b
kousuke <kousuke.econ15060709@hotmail.co.jp>
2015-08-04 21:19:05 +0900

    change index.css

--- app/webroot/gulpfile.js
+++ app/webroot/gulpfile.js
@@ -3,14 +3,14 @@ var sass = require('gulp-sass');
 var plumber = require('gulp-plumber');
 
 gulp.task('sass', function(){
-    gulp.src('./sass/*.sass')
-        .pipe(plumber())
+    gulp.src('./sass/*.scss')
+
         .pipe(sass())
         .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function(){
-    gulp.watch('./sass/*.sass', ['sass']);
+    gulp.watch('./sass/*.scss', ['sass']);
 });
 
 gulp.task('default', ['sass','watch']);
