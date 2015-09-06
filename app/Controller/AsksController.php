cb20ba3e20a52c5260d2a455023eb31e0d0fc267
kousuke <kousuke.econ15060709@hotmail.co.jp>
2015-09-07 02:03:22 +0900

    make asks

--- app/Controller/AsksController.php
+++ app/Controller/AsksController.php
@@ -3,14 +3,30 @@ App::uses('AppController', 'Controller');
 /**
  * Asks Controller
  *
+ * @property Ask $Ask
+ * @property PaginatorComponent $Paginator
+ * @property SessionComponent $Session
  */
 class AsksController extends AppController {
 
 /**
- * Scaffold
+ * Helpers
  *
- * @var mixed
+ * @var array
  */
-	public $scaffold;
+	public $helpers = array('Text', 'Html', 'Time');
 
+/**
+ * Components
+ *
+ * @var array
+ */
+	public $components = array('Paginator', 'Session');
+
+	public function index(){
+
+	}
+	public function add(){
+
+	}
 }
